171811ab38d0c5f057360194d5227c3daaef90f5
Florian Seidl <fs@scale.sc>
2023-04-19 19:15:42 +0200

    Fixes

--- __checks__/homepage.spec.ts
+++ __checks__/homepage.spec.ts
@@ -53,6 +53,9 @@ test.describe("Random Shop Category Checkout Test", () => {
         // Add the product to the cart
         await page.click("button.btn-buy")
 
+        // Take a screenshot of the current page
+        await page.screenshot({ path: "buy" + '.jpg' })
+
         // Go to the checkout page
         await page.goto(process.env.BASE_URL+"/checkout/register")
 
@@ -60,19 +63,34 @@ test.describe("Random Shop Category Checkout Test", () => {
         await page.screenshot({ path: "checkout" + '.jpg' })
 
         // Fill in the checkout form
-        await page.getElementById("#salutationId").selectOption({ label: "Mr." })
-        await page.fill("#firstName", "John")
-        await page.fill("#lastName", "Doe")
-        await page.getElementById("#guest").check()
-        await page.fill("#email", "john.doe@scale.sc")
-        await page.fill("#billingAddress[street]", "Hermann-Str. 38")
-        await page.fill("#billingAddress[zipcode]", "81369")
-        await page.fill("#billingAddress[city]", "Munich")
+        await page.locator('#personalSalutation').selectOption({ label: "Mr." });
+        //await page.getElementById("#salutationId").selectOption({ label: "Mr." })
+        await page.locator("#personalFirstName").fill("John")
+        await page.locator("#personalLastName").fill("Doe")
+        await page.locator('body > main > div > div > div > div > div.checkout-main > div.card.register-card > div > form > div.register-personal > div.custom-control.custom-checkbox.register-guest-control > label').check();
+        //await page.getByRole('checkbox', { id: 'guest' }).check();
+        //await page.getElementById("#guest").check()
+        await page.locator("#personalMail").fill("john.doe@scale.sc")
+        await page.locator("#billingAddressAddressStreet").fill("Hermann-Str. 38")
+        await page.locator("#billingAddressAddressZipcode").fill("80339")
+        await page.locator("#billingAddressAddressCity").fill("Munich")
+        await page.locator('#billingAddressAddressCountry').selectOption({ label: "Germany" });
 
         // Submit the checkout form
-        await page.click('input[type="submit"]')
+        await page.click('body > main > div > div > div > div > div.checkout-main > div.card.register-card > div > form > div.register-submit > button')
 
-        // Verify that the checkout was successful
-        //await page.waitForSelector('.checkout-success');
+        // Wait for the confirmation page to load
+        await page.waitForSelector('#confirmFormSubmit');
+        await page.screenshot({ path: "confirmation" + '.jpg' })
+
+        // Accept AGBs
+        await page.locator('body > main > div > div > div > div > div.checkout-main > div.confirm-tos > div > div > div.custom-control.custom-checkbox > label').check();
+
+        // Submit the confirmation form
+        await page.click("#confirmFormSubmit")
+
+        await page.locator('.finish-header').waitFor()
+        await page.screenshot({ path: "done" + '.jpg' })
     })
+
 })
